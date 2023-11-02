c15f40503284a28c31e367cb9aff278ddc9c5178
SandrineFaucher <sandrinefaucher2606@gmail.com>
2023-11-02 19:16:20 +0100

    Appel API météo

---
+++ meteo.js
@@ -0,0 +1,39 @@
+let image = document.getElementById("image");
+let temperature = document.getElementById("temperature");
+let ville = document.getElementById("ville");
+let ressenti = document.getElementById("ville");
+let humidity = document.getElementById("humidity");
+
+
+const appelMeteo = async () => {
+    let request = "https://api.openweathermap.org/data/2.5/weather?lat=48.85&lon=2.32&units=metric&appid=3049f73bb843072f50124411dc8e0640";
+  
+    try {
+      let response = await fetch(request);
+      if (response.ok) {
+        let data = await response.json();
+
+        // Mise à jour des éléments HTML avec les données
+      image.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      temperature.textContent = `${data.main.temp} °C`;
+      ville.textContent = data.name;
+      ressenti.textContent = `${data.main.feels_like} °C`;
+      humidity.textContent = `${data.main.humidity}%`;
+        console.log(data);
+
+        
+      } else {
+        console.error('Erreur lors de la requête');
+      }
+    } catch (error) {
+      console.error('Une erreur s\'est produite', error);
+    }
+    image.src = 
+    temperature.textContent = data.main.name;
+  }
+  appelMeteo()
+  
+  
+  
+  
+  
